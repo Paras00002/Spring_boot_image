82620cf16025528b2a586d944a23c7121b1db0d8
Paras-003 <[parastomar328@gmail.com]>
2023-10-10 08:13:01 +0530

    First commit

--- ImageOperation/src/main/java/com/coforge/ImageOperation/services/FileService.java
+++ ImageOperation/src/main/java/com/coforge/ImageOperation/services/FileService.java
@@ -3,12 +3,16 @@ package com.coforge.ImageOperation.services;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 public interface FileService {
  String uploadImage(String path, MultipartFile file) throws IOException;
 
 // path -> actual path where we want to upload , MultipartFile->data
+
+ InputStream getResource(String path,String fileName) throws FileNotFoundException;
 
 }
